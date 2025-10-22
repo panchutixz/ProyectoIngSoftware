4d5262a99d0d605599a5caf1add2ecea662665cc
Panchutizx <francisco.matamal2301@alumnos.ubiobio.cl>
2025-10-22 19:43:34 -0300

    agrego Backend Y frontend

---
+++ backend/src/routes/index.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import authRoutes from "./auth.routes.js";
+import profileRoutes from "./profile.routes.js";
+
+export function routerApi(app) {
+  const router = Router();
+  app.use("/api", router);
+  router.use("/auth", authRoutes);
+  router.use("/profile", profileRoutes);
+}
