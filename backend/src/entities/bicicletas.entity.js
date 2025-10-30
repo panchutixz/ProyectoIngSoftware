8acd5f4d818b75c745a07a94392e9b5f23b3f1b1
Panchutizx <francisco.matamal2301@alumnos.ubiobio.cl>
2025-10-29 23:44:59 -0300

    añadido barbara

---
+++ backend/src/entities/bicicletas.entity.js
@@ -0,0 +1,71 @@
+import { EntitySchema, JoinColumn } from "typeorm";
+
+export const Bicicleta = new EntitySchema({
+    name: "Bicicleta",
+    tableName: "bicicletas",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: "increment",
+        },
+        marca: {
+            type: "varchar",
+            length: 100,
+            nullable: false,
+        },
+        color: {
+            type: "varchar",
+            length: 40,
+            nullable: false,
+        },
+        numero_serie: {
+            type: "varchar",
+            length: 100,
+            unique: true,
+        },
+        descripcion: {
+            type: "varchar",
+            length: 100,
+            nullable: false,
+        },
+        estado: {
+            type: "varchar",
+            length: 100,
+            nullable: false,
+        },
+
+        createdAt: {
+            type: "timestamp",
+            default: () => "CURRENT_TIMESTAMP",
+        },
+
+        updateAt: {
+            type: "timestamp",
+            default: () => "CURRENT_TIMESTAMP",
+            onUpdate: () => "CURRENT_TIMESTAMP",
+        },
+    },
+    relations: {
+        users: {
+            type: "one-to-one",
+            target: "User",
+            JoinColumn: {name: "rut_user"},
+            eager: true,
+            nullable: false,
+        },
+        bicicletero: {
+            type: "one-to-one",
+            target: "Bicicletero",
+            JoinColumn: {name: "id"},
+            eager: true,
+            nullable: false,
+
+        }
+
+    },
+
+
+});
+//ola
+export default Bicicleta;
