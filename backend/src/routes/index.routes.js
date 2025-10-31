ff71c172a6b4e4806ce25bffa4d21beb1712d26a
Panchutizx <francisco.matamal2301@alumnos.ubiobio.cl>
2025-10-30 22:14:25 -0300

    .

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import profileRoutes from "./profile.routes.js";
+import userRoutes from "./user.routes.js";
 
 export function routerApi(app) {
   const router = Router();
   app.use("/api", router);
   router.use("/auth", authRoutes);
   router.use("/profile", profileRoutes);
+  router.use("/users", userRoutes);
 }
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { getUsers, getUserById, createUser, deleteUserById, updateUserById } from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.get("/", authMiddleware, getUsers);
+router.get("/:id", authMiddleware, getUserById);
+router.post("/", createUser);
+router.put("/:id", authMiddleware, updateUserById);
+router.delete("/:id", authMiddleware, deleteUserById);
+export default router;
