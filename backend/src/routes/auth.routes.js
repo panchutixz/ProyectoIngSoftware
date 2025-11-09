389e76e37407bf9f2a979fae423049f2f39e2b2c
Panchutizx <francisco.matamal2301@alumnos.ubiobio.cl>
2025-11-09 20:05:01 -0300

    añadido barbara y pame y mari

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller.js";
 import { registerBicycle, getBicycle, retirarBicycle } from "../controllers/bicicletas.controller.js";
-import { getHistoryByUser } from "../controllers/history.controller.js";
+import { getHistoryByUser } from "../controllers/historial.controller.js";
 import { createBikeRack, getAllBikeRacks, getBikeRackById, updateBikeRack, deleteBikeRack, asignarGuardia, desasignarGuardia} from "../controllers/bicicletero.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
