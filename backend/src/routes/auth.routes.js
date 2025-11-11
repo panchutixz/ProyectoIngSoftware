b17ed0b70f4d60db8994c4b75131c9c9202c656a
Maria Paz Poveda <maria.poveda2301@alumnos.ubiobio.cl>
2025-11-11 00:44:47 -0300

    funcionando correctamente tambien funcion de que el usuario obtenga informacion de su bicicleta

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller.js";
-import { registerBicycle, getBicycle, retirarBicycle } from "../controllers/bicicletas.controller.js";
+import { registerBicycle, getBicycle, retirarBicycle, getUserBicycles } from "../controllers/bicicletas.controller.js";
 import { getHistoryByUser } from "../controllers/historial.controller.js";
 import { createBikeRack, getAllBikeRacks, getBikeRackById, updateBikeRack, deleteBikeRack, asignarGuardia, desasignarGuardia} from "../controllers/bicicletero.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
-
 
 router.post("/login", login);
 router.post("/register", register);
@@ -22,5 +21,6 @@ router.patch("/update/bicicletero", updateBikeRack);
 router.delete("/delete/bicicletero", deleteBikeRack);
 router.post("/asignar/bicicletero", authMiddleware, asignarGuardia);
 router.post("/historial/rut", authMiddleware, getHistoryByUser);
+router.get("/usuario/:rut", authMiddleware, getUserBicycles);
 
 export default router;
