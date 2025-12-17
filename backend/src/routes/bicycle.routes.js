7feee98b7595b4542ecf6f1b29ff391e92b03af5
Barbara Inostroza~ <antonia.inostroza711@gmail.com~>
2025-12-17 17:15:06 -0300

    funcionando reingreso bicis

--- backend/src/routes/bicycle.routes.js
+++ backend/src/routes/bicycle.routes.js
@@ -5,7 +5,7 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.post("/bicicleta", registerBicycle);
-router.post("/reingreso", reIngresoBicycle);
+router.patch("/reingreso", reIngresoBicycle);
 router.get("/obtener", authMiddleware, getBicycle);
 router.get("/usuario/:rut", getUserBicycles);
 router.delete("/retirar", authMiddleware, retirarBicycle);
