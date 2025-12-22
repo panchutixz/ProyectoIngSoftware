9247c85f5152493bcc0b6dc0de6330f47aa33821
Barbara Inostroza~ <antonia.inostroza711@gmail.com~>
2025-12-22 19:45:37 -0300

    agregado las restricciones de role, falta arreglar una cosa visual

--- frontend/src/pages/Bicicletas.jsx
+++ frontend/src/pages/Bicicletas.jsx
@@ -4,8 +4,10 @@ import { getBicicletas} from '../services/bicicletas.service.js';
 import { reIngresoBicicleta } from '@hooks/bicicletas/useReIngresoBicicletas.jsx';
 import { registerBicicleta } from '@hooks/bicicletas/useRegisterBicicletas.jsx';
 import { retirarBicicletas } from '@hooks/bicicletas/useRetirarBicicletas.jsx';
+import { useAuth } from "../context/AuthContext.jsx";
 
 const Bicicletas = () => {
+    const { user } = useAuth();
     const [bicicletas, setBicicletas] = useState([]);
     const [error] = useState(null);
 
@@ -28,50 +30,53 @@ const Bicicletas = () => {
     const { handleRetirarBicicleta } = retirarBicicletas(fetchBicicletas);
 
     return (
-    <div className="bicicletas-page">
-        <div className="bicicletas-header">
-            <h1 className="title-listar-bicicletas">Listado de Bicicletas</h1>
-            <button className="button-registrar-bicicleta" onClick={handleRegisterBicicleta}>Registrar Bicicleta</button>
-            <button className="button-retirar-bicicleta" onClick={handleRetirarBicicleta}>Retirar Bicicleta</button>
-        </div>
-    <table className="bicicleta-table">
-    <thead>
-    <tr>
-        <th>Bicicletero</th>
-        <th>Marca</th>
-        <th>Color</th>
-        <th>Número Serie</th>
-        <th>Código</th>
-        <th>Descripción</th>
-        <th>Estado</th>
-        <th>Rut Usuario</th>
-        <th>Re-Ingresar</th>
-        
-    </tr>
-    </thead>
-    <tbody>
+        <div className="bicicletas-page">
+            <div className="bicicletas-header">
+                <h1 className="title-listar-bicicletas">Listado de Bicicletas</h1>
+                {user && user.rol === 'Guardia' && (
+                    <>
+                        <button className="button-registrar-bicicleta" onClick={handleRegisterBicicleta}>Registrar Bicicleta</button>
+                        <button className="button-retirar-bicicleta" onClick={handleRetirarBicicleta}>Retirar Bicicleta</button>
+                    </>
+                )}
+            </div>
 
-    {bicicletas.map((bici) => (
-        <tr key={bici.id}>
-        <td>{bici.bicicletero.nombre}</td>
-        <td>{bici.marca}</td>
-        <td>{bici.color}</td>
-        <td>{bici.numero_serie}</td>
-        <td>{bici.codigo}</td>
-        <td>{bici.descripcion}</td>
-        <td>{bici.estado}</td>
-        <td>{bici.usuario.rut}</td>
-        <td>
-            <button className="btn-icon" onClick={handleReIngresoBicicleta}>
-            <i className="fa-solid fa-arrow-right-to-bracket"></i>
-            </button>
-        </td>
-        </tr>
-    ))}
-    </tbody>
-</table>
-    {error && <p className="error-message">{error}</p>}
-    </div>
+            <table className="bicicleta-table">
+                <thead>
+                    <tr>
+                        <th>Bicicletero</th>
+                        <th>Marca</th>
+                        <th>Color</th>
+                        <th>Número Serie</th>
+                        <th>Código</th>
+                        <th>Descripción</th>
+                        <th>Estado</th>
+                        <th>Rut Usuario</th>
+                        {user && user.rol === 'Guardia' && <th>Re-Ingresar</th>}
+                    </tr>
+                </thead>
+                <tbody>
+                    {bicicletas.map((bici) => (
+                        <tr key={bici.id}>
+                            <td>{bici.bicicletero.nombre}</td>
+                            <td>{bici.marca}</td>
+                            <td>{bici.color}</td>
+                            <td>{bici.numero_serie}</td>
+                            <td>{bici.codigo}</td>
+                            <td>{bici.descripcion}</td>
+                            <td>{bici.estado}</td>
+                            <td>{bici.usuario.rut}</td>
+                            <td>
+                                {user.rol === 'Guardia' && ( <button className="btn-icon" onClick={handleReIngresoBicicleta}> 
+                                    <i className="fa-solid fa-arrow-right-to-bracket"></i> </button> )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            {error && <p className="error-message">{error}</p>}
+        </div>
     );
 }
     
