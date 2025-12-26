c1c5bc87677fd83ceda0a7b6642efc088192311b
Barbara Inostroza~ <antonia.inostroza711@gmail.com~>
2025-12-26 13:06:28 -0300

    revisar refresh olvidada

--- frontend/src/hooks/bicicletas/useGetBicicletas.jsx
+++ frontend/src/hooks/bicicletas/useGetBicicletas.jsx
@@ -1,37 +1,19 @@
 import { useState } from "react";
 import { getBicicletas } from "../../services/bicicletas.service.js";
-import Swal from "sweetalert2";
 
 export const useGetBicicletas = () => {
     const [bicicletas, setBicicletas] = useState([]);
 
     const fetchBicicletas = async () => {
-        try{
+        try {
             const response = await getBicicletas();
-                setBicicletas(response.data || []); 
-            }catch(error){
-                console.error("Error al cargar las bicicletas:", error);
-                const mensaje = error?.response?.data?.message;
-            if (mensaje === "Guardia sin bicicletero asignado") {
-                Swal.fire({
-                    icon: 'warning',
-                    title: 'Acceso restringido',
-                    text: 'No tienes un bicicletero asignado. Por favor, contacta al administrador.',
-                    confirmButtonText: 'Aceptar'
-                });
-                setBicicletas();
-                return;
-            }
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: mensaje || 'Ha ocurrido un error al cargar las bicicletas. Inténtalo de nuevo más tarde.',
-                confirmButtonText: 'Aceptar'
-            });
+            setBicicletas(response.data || []);
+        } catch (error) {
+        console.error("Error al cargar las bicicletas:", error);
             setBicicletas([]);
         }
-        
+    };
     return { bicicletas, fetchBicicletas };
-    }
 };
+
 export default useGetBicicletas;
